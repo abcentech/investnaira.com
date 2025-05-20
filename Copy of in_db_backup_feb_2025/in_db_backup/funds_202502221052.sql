138d4a1d53acd1f113885fe47b5f1282c03866fd
abcentech <abcentech@gmail.com>
2025-05-20 17:52:18 +0100

    Add files via upload

---
+++ Copy of in_db_backup_feb_2025/in_db_backup/funds_202502221052.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.funds (fund_name,created_at,updated_at) VALUES
+	 ('conservative','2019-10-02 15:17:21+01','2019-10-02 15:17:21+01'),
+	 ('balanced','2019-10-02 15:17:21+01','2019-10-02 15:17:21+01'),
+	 ('aggressive','2019-10-02 15:17:22+01','2019-10-02 15:17:22+01');
---
+++ Copy of in_db_backup_feb_2025/in_db_backup/tasks_202502221052.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.tasks (id,user_id,"name","type",frequency,reward,starts_at,completion_level,created_at,updated_at) VALUES
+	 ('8f8dde0e-0fd4-4bbf-9c2b-b1f920f80915','3e27a539-4318-4a07-ae47-22bfb37b45ee','Room Keeping','room keeping',1,50.0,'0',1,'2022-07-18 19:06:05+01','2022-07-18 19:06:05+01');
